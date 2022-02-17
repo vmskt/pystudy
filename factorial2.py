246597356845daa5ef910536a0a0b17b65c845b9
vmskt <vmonwork@gmail.com>
2022-02-17 15:21:05 +0300

    Конечный вариант, надеюсь

--- factorial2.py
+++ factorial2.py
@@ -1,14 +1,12 @@
-userInput = input('Введите целое положительное число: ')   # userInput - строковая пер-ная для ввода числа;
-def count_zeros(n: str) -> str:     # объявили функ-ю согласно замечанию;
-    if not n.isdigit():   # проверили на целостность введёное значение - isdigit вообще тема!
-        return 'TypeError: введеное значение не является целым числом.'
-    elif int(n) < 0:    # проверили на отрицательное число преобразовав в тип int
-        return 'ValueError: введено отрицательное число. Не надо так.'
-    else:   # основное тело функции
-        int_variable = int(n)
-        res = 0
-        while int_variable > 0:
-            int_variable //= 5
-            res += int_variable
-        return str(res) + ' нулей в конце факториала'   # сделали res строкой -выполнили условие объявления функ-ии
-print(count_zeros(userInput))
+def count_zeros(n: int) -> int:
+    if not isinstance(n, int): # != int:
+        raise TypeError("Написано же: целое число вводить нужно...")
+    if n < 0:
+        raise ValueError("Введено отрицательное число. Не надо так...")
+    res = 0
+    while n > 0:
+        n //= 5
+        res += n
+    return res
+
+
